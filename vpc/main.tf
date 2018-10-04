14eaecbccc74fe2b3db8338f95dce3dab5f0b101
root <root@ip-10-0-0-35.ec2.internal>
2018-10-04 17:56:14 +0000

    ok

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,22 +4,28 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+############## VPC creation ###############
+
 resource "aws_vpc" "main" {
   cidr_block = "10.10.0.0/16"
+  tags {
+    Name = "Terraform VPC"
+  }
 }
-
+##################################################
+############# Public Subnet Creation #############
+##################################################
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "10.10.1.0/24"
   map_public_ip_on_launch = true
+  tags {
+     Name = "Public_Subnet"
+  }
 }
+
 resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_vpc.main.id}"
-}
-resource "aws_route" "internet_access" {
-  route_table_id         = "${aws_vpc.main.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.default.id}"
 }
 
 resource "aws_route_table" "public" {
@@ -31,12 +37,52 @@ resource "aws_route_table" "public" {
   }
 
   tags {
-    Name = "Public"
+    Name = "Public_Route_Table"
   }
 }
 
 resource "aws_route_table_association" "public" {
-  subnet_id	 = "${aws_subnet.public_subnet.id}"  # How to put pub_subnet_azc.id into here?
+  subnet_id	 = "${aws_subnet.public_subnet.id}" 
   route_table_id = "${aws_route_table.public.id}"
+}
+
+##################################################
+############# Private Subnet Creation #############
+##################################################
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "10.10.2.0/24"
+  tags {
+     Name = "Private_Subnet"
+  }
+}
+
+resource "aws_eip" "nat" {
+  vpc = "True"
+  tags {
+    Name = "gw_NAT"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat.id}"
+  subnet_id     = "${aws_subnet.private_subnet.id}"
+}
+
+resource "aws_route_table" "private_route" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+  tags {
+    Name = "Private_Route_Table"
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id	 = "${aws_subnet.private_subnet.id}"  
+  route_table_id = "${aws_route_table.private_route.id}"
 }
 
