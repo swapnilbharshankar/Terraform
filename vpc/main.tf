6fd9e6b2105a59e2290d9107b4e540e670e32280
root <root@ip-10-0-0-35.ec2.internal>
2018-10-04 12:51:04 +0000

    ok

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,33 +1,42 @@
 provider "aws" {
-  region = "eu-west-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region     = "${var.region}"
 }
 
-module "vpc" {
-  source = "../../"
+resource "aws_vpc" "main" {
+  cidr_block = "10.10.0.0/16"
+}
 
-  name = "simple-example"
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "${aws_vpc.main.id}"
+  cidr_block              = "10.10.1.0/24"
+  map_public_ip_on_launch = true
+}
+resource "aws_internet_gateway" "default" {
+  vpc_id = "${aws_vpc.main.id}"
+}
+resource "aws_route" "internet_access" {
+  route_table_id         = "${aws_vpc.main.main_route_table_id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.default.id}"
+}
 
-  cidr = "10.0.0.0/16"
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  assign_generated_ipv6_cidr_block = true
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.default.id}"
   }
 
-  tags = {
-    Owner       = "user"
-    Environment = "dev"
-  }
-
-  vpc_tags = {
-    Name = "vpc-name"
+  tags {
+    Name = "Public"
   }
 }
+
+resource "aws_route_table_association" "public" {
+  subnet_id	 = "${aws_subnet.public_subnet.id}"  # How to put pub_subnet_azc.id into here?
+  route_table_id = "${aws_route_table.public.id}"
+}
+
